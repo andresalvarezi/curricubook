37caf774ec7fd39ee17d7f3a10d9a2f4a0aa166c
Andrés Álvarez Iglesias <aalvarez@sampol.com>
2025-05-12 13:59:52 +0200

    Reorganization

--- appimage/generate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ../curricubook
-uv run python-appimage build app -p 3.12 ../appimage/recipe
-mv curricubook-x86_64.AppImage ../appimage
---
+++ curricubook/appimage.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+uv run python-appimage build app -p 3.12 ./appimage
+
